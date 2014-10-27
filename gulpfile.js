112a7d28dde2e33d9e9588757ba3a8f824c99b32
Thomas D Keven <thomasdkleven@gmail.com>
2014-10-27 11:32:40 -0600

    added Gulp and GUtil

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+// gulpfile.js
+var gulp = require('gulp'),
+	gutil = require('gulp-util');
+
+// tasks 
+gulp.task('log' , function(){
+	// this is an added function command 
+	gutil.log('workflows Rock');
+});
